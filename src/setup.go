a5f6cf32192de29c59edf9274bbfa94cfc104b78
gmemstr <bladesimmer@gmail.com>
2017-09-21 12:10:59 -0700

    Skeleton files for setup process & user (new) management

---
+++ src/setup.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Serve setup.html and config parameters
+func ServeSetup(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Write JSON config to file
+func WriteConfig() {
+
+}
+
+// Connect to SQL and create admin user
+func CreateAdmin() {
+
+}
