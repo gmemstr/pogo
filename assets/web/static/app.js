454006b8e568bb8cfdb17e9c1f966513e189ebb7
gmemstr <bladesimmer@gmail.com>
2017-10-06 13:24:05 -0700

    Move to vue-router
    Implemented vue-router w/ data fetching, plan to make it lazy at some point.

--- assets/web/static/app.js
+++ assets/web/static/app.js
@@ -1,46 +1,67 @@
-Vue.component('episode-publish-form', {
-	template: '<form enctype="multipart/form-data" action="/admin/publish" method="post"><label for="title">Episode Title</label><input type="text" id="title" name="title"><label for="description">Episode Description</label><textarea name="description" id="description" cols="100" rows="20" style="resize: none;"></textarea><label for="file">Media File</label><input type="file" id="file" name="file"><label for="date">Publish Date</label><input type="date" id="date" name="date"><input type="submit" value="Publish"></form>'
-})
-
-Vue.component('custom-css', {
-	template: '<form action="/admin/css" method="post" enctype="multipart/form-data"><label for="css">Custom CSS</label><textarea name="css" id="css" cols="120" rows="20"></textarea><br /><input type="submit" value="Submit"></form>'
-})
-
-var app = new Vue({
-  el: '#app',
-  data: {
-    header: 'Pogo Admin',
-    current_page: "Page Title",
-    page: false,
-  }
-})
-
-window.onhashchange = setpagecontents
-window.onload = setpagecontents
-// I know I'm probably not using 
-// vue.js properly here but it's the
-// best I can do right now
-function setpagecontents(){
-	page = window.location.href.split('#')[1]
-	app.page = page
-	
-	if (page == "publish") {
-		app.current_page = "Publish Episode"
-	} 
-	else if (page == "customcss") {
-		app.current_page = "Edit Theme"
-		getcss()
-	} 
-	else {
-		app.current_page = "404 Not found"
-	}
+const episodepublishform = {
+    template: '<form enctype="multipart/form-data" action="/admin/publish" method="post"><label for="title">Episode Title</label><input type="text" id="title" name="title"><label for="description">Episode Description</label><textarea name="description" id="description" cols="100" rows="20" style="resize: none;"></textarea><label for="file">Media File</label><input type="file" id="file" name="file"><label for="date">Publish Date</label><input type="date" id="date" name="date"><input type="submit" value="Publish"></form>',
+    current_page: "Publish Episode"
 }
 
-function getcss(){
-	var xmlHttp = new XMLHttpRequest();
+const customcss = {
+    template: '<form action="/admin/css" method="post" enctype="multipart/form-data"><label for="css">Custom CSS</label><textarea name="css" id="css" cols="120" rows="20">{{ css }}</textarea><br /><input type="submit" value="Submit"></form>',
+    current_page: "Publish Episode",
+    data() {
+        return {
+            loading: false,
+            css: null,
+            error: null
+        }
+    },
+    created() {
+        // fetch the data when the view is created and the data is
+        // already being observed
+        this.fetchData()
+    },
+    watch: {
+        // call again the method if the route changes
+        '$route': 'fetchData'
+    },
+    methods: {
+        fetchData() {
+            this.error = this.css = null
+            this.loading = true
+
+            getCss( (err, css) => {
+                this.loading = false
+                if (err) {
+                    this.error = err.toString()
+                } else {
+                    this.css = css
+                }
+            })
+        }
+    }
+}
+
+const routes = [
+    { path: '/publish', component: episodepublishform },
+    { path: '/theme', component: customcss }
+]
+
+const router = new VueRouter({
+    routes
+})
+
+const app = new Vue({
+    router,
+    data: {
+        header: 'Pogo Admin',
+        current_page: "Page Title",
+        page: false,
+    }
+}).$mount('#app')
+
+function getCss(callback) {
+    var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            document.getElementById("css").innerHTML=xmlHttp.responseText;
+            callback(null, xmlHttp.responseText)
     }
     xmlHttp.open("GET", "/admin/css", true);
     xmlHttp.send(null);
