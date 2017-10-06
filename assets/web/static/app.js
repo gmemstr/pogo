ad698b78012233ddcf581e24d8594c8603bbf8f0
gmemstr <bladesimmer@gmail.com>
2017-10-06 14:13:30 -0700

    Move current page to template.

--- assets/web/static/app.js
+++ assets/web/static/app.js
@@ -1,10 +1,10 @@
 const episodepublishform = {
-    template: '<form enctype="multipart/form-data" action="/admin/publish" method="post"><label for="title">Episode Title</label><input type="text" id="title" name="title"><label for="description">Episode Description</label><textarea name="description" id="description" cols="100" rows="20" style="resize: none;"></textarea><label for="file">Media File</label><input type="file" id="file" name="file"><label for="date">Publish Date</label><input type="date" id="date" name="date"><input type="submit" value="Publish"></form>',
+    template: '<div><h3>Publish Episode</h3><form enctype="multipart/form-data" action="/admin/publish" method="post"><label for="title">Episode Title</label><input type="text" id="title" name="title"><label for="description">Episode Description</label><textarea name="description" id="description" cols="100" rows="20" style="resize: none;"></textarea><label for="file">Media File</label><input type="file" id="file" name="file"><label for="date">Publish Date</label><input type="date" id="date" name="date"><input type="submit" value="Publish"></form></div>',
     current_page: "Publish Episode"
 }
 
 const customcss = {
-    template: '<form action="/admin/css" method="post" enctype="multipart/form-data"><label for="css">Custom CSS</label><textarea name="css" id="css" cols="120" rows="20">{{ css }}</textarea><br /><input type="submit" value="Submit"></form>',
+    template: '<div><h3>Edit CSS</h3><form action="/admin/css" method="post" enctype="multipart/form-data"><label for="css">Custom CSS</label><textarea name="css" id="css" cols="120" rows="20">{{ css }}</textarea><br /><input type="submit" value="Submit"></form></div>',
     current_page: "Publish Episode",
     data() {
         return {
@@ -52,8 +52,6 @@ const app = new Vue({
     router,
     data: {
         header: 'Pogo Admin',
-        current_page: "Page Title",
-        page: false,
     }
 }).$mount('#app')
 
